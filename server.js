b191934756ce0690d167f50ef02b44339d26db5c
Abhi Namballa <namballaabhi@gmail.com>
2020-03-20 22:36:53 -0400

    commit on master

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app = express();
 
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri,{
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 app.get('/',(req,res) =>{
-    res.render('index')
+    const shortUrls = ShortUrl.find()
+    res.render('index', {shortUrls: shortUrls})
+})
+
+app.post('/shortUrls', (req,res)=>{
+  ShortUrl.create({ full: req.body.FullUrl })
+
+ res.redirect('/')
 })
 app.listen(process.env.PORT || 5000);
 
